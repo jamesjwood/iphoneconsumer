3cd07fe1d120eeded46b4df04e2034163f24594b
jamesjwood <jamesjwood@msn.com>
2014-07-13 15:19:20 +0100

    Initial Commit

---
+++ iphoneconsumer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iphoneconsumer
+//
+//  Created by James Wood on 13/07/2014.
+//  Copyright (c) 2014 James Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
